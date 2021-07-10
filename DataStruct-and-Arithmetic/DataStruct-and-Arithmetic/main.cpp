c940a025fcafafa16791f185d4c1b84cf4327a09
engili <lmt.coding@gmail.com>
2021-07-10 14:22:21 +0800

    first init

---
+++ DataStruct-and-Arithmetic/DataStruct-and-Arithmetic/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  DataStruct-and-Arithmetic
+//
+//  Created by Mengtian Li on 2021/7/10.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
